adcf0d27a149aa5f729b56a11f5b75d9c06744d4
jeonsumin <51107183+jeonsumin@users.noreply.github.com>
2022-01-11 11:26:18 +0900

    commit

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet var minTempLabel: UILabel!
     @IBOutlet var weatherStackView: UIStackView!
     
-    //MARK: -LifeCycle
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -35,21 +35,27 @@ class ViewController: UIViewController {
     //MARK: - Function
     func getCurrentWeather(cityName : String ){
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=bd739ffb0fc5425c5b1731d209334352") else { return }
+        // URLSession 선언
         let session = URLSession(configuration: .default)
+        // dataTask를 통해 URL Data 가져오기
         session.dataTask(with: url) {[weak self] data, response, error  in
             let successRang = (200..<300)
             guard let data = data, error == nil else { return }
+            // json decoder 선언 
             let decoder = JSONDecoder()
             if let response = response as? HTTPURLResponse, successRang.contains(response.statusCode) {
+                // struct Codable을 통해 dataTask의 json형태의 데이터를 weatherInfomation 객체로 decode
                 guard let weatherInfomation = try? decoder.decode(WeatherInfomotion.self, from: data) else { return }
                 
+                //View 관련 로직 처리는 Main 스레드에서 진행하여 하기 때문에 
                 DispatchQueue.main.async {
                     self?.weatherStackView.isHidden = false
                     self?.configureView(weatherInfomation: weatherInfomation)
                 }
             }else {
                 guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
-                
+
+                //View 관련 로직 처리는 Main 스레드에서 진행하여 하기 때문에 
                 DispatchQueue.main.async {
                     self?.showAlert(message: errorMessage.message)
                 }
@@ -57,7 +63,7 @@ class ViewController: UIViewController {
             
         }.resume()
     }
-    
+    //화면 구성
     func configureView(weatherInfomation: WeatherInfomotion){
         cityNameLabel.text = weatherInfomation.name
         if let weather = weatherInfomation.weather.first {
@@ -68,9 +74,15 @@ class ViewController: UIViewController {
         maxTempLabel.text = "최고: \(Int(weatherInfomation.temp.maxTemp - 273.15))°C"
     }
     
+    //Error Alert 
     func showAlert(message: String){
+        //AlertController 선언 
         let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        
+        //AlertController에서 사용할 Action 추가
         alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+
+        //화면으로 전달
         present(alert, animated: true , completion: nil )
     }
 }
